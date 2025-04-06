58caae74ad176a7fbb44e499856c35a0d1fb79c2
Ayesha Iqbal <ayesha.iqbal2105@gmail.com>
2025-04-06 20:33:41 +0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#8B4513]">
+      <h1 className="text-4xl font-bold text-white mb-6">Hello Tailwind!</h1>
+      <div className="bg-red-500 p-10 rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold text-white">Tailwind Test</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
